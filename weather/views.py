0048481cb712ccd6af495d8c7df9e17973ec0574
Rõhit Telgote <109582196+rohittelgote23@users.noreply.github.com>
2024-02-25 12:34:19 +0530

    delete previous city

--- weather/views.py
+++ weather/views.py
@@ -9,6 +9,7 @@ def index(request):
 
     if request.method == 'POST': # only true if form is submitted
         form = CityForm(request.POST) # add actual request data to form for processing
+        City.objects.all().delete()
         form.save() # will validate and save if validate
 
     form = CityForm()
